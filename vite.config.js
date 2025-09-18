8e962ea1185f9f5af8da3fd154209c3a36c6a86c
ShmuelBarazani <tropikan1@gmail.com>
2025-09-18 08:12:04 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// חשוב: base חייב להיות הנתיב של הריפו שלך בגיטהאב, כולל הסלאשים משני הצדדים
-// במקרה שלך: /KATREGEL-Gan-Daniel/
 export default defineConfig({
   plugins: [react()],
-  base: '/KATREGEL-Gan-Daniel/',
+  base: '/', // אל תשים תת-תיקייה. ב-Vercel זה חייב להיות '/' (או להסיר את המפתח)
 })
