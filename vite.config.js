b9f5e7e2003c37a45c534f13f7d76fc9e96ca139
ShmuelBarazani <tropikan1@gmail.com>
2025-09-18 08:39:51 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // אל תשים תת-תיקייה. ב-Vercel זה חייב להיות '/' (או להסיר את המפתח)
+  base: '/', // חשוב: לא לשים תת-נתיב
 })
